b548ed6d3484cd21f151c7c661ab2b825d8d9b53
ebrahim-sani <ibraheemsani035@gmail.com>
2022-04-08 01:01:39 +0100

    ..

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,10 @@ const Home = ({ collections }: Props) => {
         <div className="grid space-x-3 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
           {collections.map((collection) => (
             <Link href={`/nft/${collection.slug.current}`}>
-              <div className="flex flex-col items-center cursor-pointer transition-all duration-200 hover:scale-105">
+              <div
+                key={collection._id}
+                className="flex flex-col items-center cursor-pointer transition-all duration-200 hover:scale-105"
+              >
                 <img
                   className="h-96 w-60 rounded-2xl object-cover"
                   src={urlFor(collection.mainImage).url()}
